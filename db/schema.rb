2f11e81d9fe0ef69cebc73fa0d95857484e292e5
Anakorn Kyavatanakij <anakornk@gmail.com>
2017-08-14 17:33:09 +0800

    story model,stories controller,pages controller implememented,apart from stories show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814064444) do
+ActiveRecord::Schema.define(version: 20170814083749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,17 @@ ActiveRecord::Schema.define(version: 20170814064444) do
     t.string "question"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "story_id"
+    t.index ["story_id"], name: "index_pages_on_story_id"
   end
 
+  create_table "stories", force: :cascade do |t|
+    t.string "title"
+    t.integer "root_page_id"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "pages", "stories"
 end
