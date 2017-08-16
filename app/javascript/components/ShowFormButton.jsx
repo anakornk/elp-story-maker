df65d71dfa105857e9573e642e468fea5d2d5868
Anakorn Kyavatanakij <anakornk@gmail.com>
2017-08-16 18:24:10 +0800

    replace parentNode with closet method

--- app/javascript/components/ShowFormButton.jsx
+++ app/javascript/components/ShowFormButton.jsx
@@ -21,7 +21,7 @@ class ShowFormButton extends React.Component {
       var copy = Object.assign({},that.props.defaultFormSettings);
 
         if(this.classList.contains("button-edit")){
-          var win = this.parentNode.parentNode.parentNode;
+          var win = this.closest('.block')
           var buttons = (win.querySelectorAll(".button"));
           //buttons shown are in orded so no need to check for choice_index
           buttons.forEach(function(button,index){
