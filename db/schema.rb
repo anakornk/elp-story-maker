f288b4c1c108bcfcdb7b55665d304794524350ca
Anakorn Kyavatanakij <anakornk@gmail.com>
2017-08-17 12:02:16 +0800

    create publish button jsx

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815142813) do
+ActiveRecord::Schema.define(version: 20170817032709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20170815142813) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "published", default: false, null: false
   end
 
   add_foreign_key "pages", "stories"
